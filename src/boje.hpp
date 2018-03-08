7e2891799c7da8c0546aa805a0fbacce15f92ba0
mustafaGlobal <37195006+mustafaGlobal@users.noreply.github.com>
2018-03-08 22:54:35 +0100

    Add files via upload

---
+++ src/boje.hpp
@@ -0,0 +1,37 @@
+#ifndef BOJE_HPP
+#define BOJE_HPP
+
+unsigned int BLACK_COLOR[3] = {0, 0, 0};
+unsigned int RED_COLOR[3] = {255, 0, 0};
+unsigned int GREEN_COLOR[3] = {0, 255, 0};
+unsigned int BLUE_COLOR[3] =  {0, 0, 255};
+unsigned int WHITE_COLOR[3] = {255, 255, 255};
+
+
+// Prvi red boja na daljinskom
+unsigned int ORANGE_COLOR[3] = {255, 69, 0};
+unsigned int YELLOW_COLOR[3] = {230, 150, 0};
+unsigned int GREEN_YELLOW_COLOR[3] = {130, 175, 0};
+unsigned int LIME_GREEN_COLOR[3] = {30, 135, 0};
+
+// Drugi red boja na daljinskom
+unsigned int TURQUOISE_COLOR[3] = {64, 224, 208};
+unsigned int LIGHTSKY_BLUE_COLOR[3] = {135, 206, 250};
+unsigned int DODGER_BLUE_COLOR[3] = {30, 144 ,255};
+unsigned int MIDNIGHT_BLUE_COLOR[3] = {25,25,112};
+
+
+// Treci red boja na daljinskom
+unsigned int VIOLET_DARK_COLOR[3] = {148, 0, 211};
+unsigned int ORCHID_MEDIUM_COLOR[3] = {186, 85 , 211};
+unsigned int MAGENTA_DARK_COLOR[3] = {139, 0 , 139};
+unsigned int PINK_DEEP_COLOR[3] = {255, 20, 147};
+
+
+unsigned int* COLORS[16]= { RED_COLOR , ORANGE_COLOR , YELLOW_COLOR , GREEN_YELLOW_COLOR , LIME_GREEN_COLOR,
+                          GREEN_COLOR , TURQUOISE_COLOR , LIGHTSKY_BLUE_COLOR , DODGER_BLUE_COLOR , MIDNIGHT_BLUE_COLOR,
+                          BLUE_COLOR , VIOLET_DARK_COLOR , ORCHID_MEDIUM_COLOR , MAGENTA_DARK_COLOR , PINK_DEEP_COLOR  };
+
+
+
+#endif
---
+++ src/daljinski.hpp
@@ -0,0 +1,29 @@
+#ifndef DALJINSKI_HPP
+#define DALJINSKI_HPP
+
+#define	IR_BPLUS    0xFF01FE	//
+#define	IR_BMINUS   0xFF817E	//
+#define	IR_OFF 	    0xFF41BE	//
+#define	IR_ON 	    0xFFC13E	//
+#define	IR_R 	      0xFF21DE  //
+#define	IR_G 	      0xFFA15E	//
+#define	IR_B 	      0xFF619E  //
+#define	IR_W 	      0xFFE11E	//
+#define	IR_B1	      0xFF11EE	//
+#define	IR_B2	      0xFF916E	//
+#define	IR_B3	      0xFF51AE  //
+#define	IR_FLASH    0xFFD12E	//
+#define	IR_B4	      0xFF31CE	//
+#define	IR_B5	      0xFFB14E	//
+#define	IR_B6	      0xFF718E	//
+#define	IR_STROBE   0xFFF10E	//
+#define	IR_B7	      0xFF09F6  //
+#define	IR_B8	      0xFF8976	//
+#define	IR_B9	      0xFF49B6	//
+#define	IR_FADE     0xFFC936	//
+#define	IR_B10	    0xFF29D6	//
+#define	IR_B11	    0xFFA956	//
+#define	IR_B12	    0xFF6996	//
+#define	IR_SMOOTH   0xFFE916	//
+
+#endif
